379d5bd3fd6becc56f5c7c705a7b688833b08519
Tünzalə <tunzale@Tuni-MacBook-Air.local>
2024-02-01 01:57:37 +0400

    added files

---
+++ scopes-functions-practice/assets/js/script.js
@@ -0,0 +1,185 @@
+"use strict";
+
+// alert("hello world");
+// let surname = "test";
+// let person = promp("Please enter your name:")
+
+// if (person = null) {
+//     console.log("invalid")
+// } else {
+//     console.log(person)
+// }
+
+
+// let elem = confirm("press a button ")
+
+// console.log(elem)
+
+
+let stu1 = {
+    id: 1,
+    name: "Tunzale",
+    surname: "Memmedova",
+    age: 24
+
+};
+
+let stu2 = {
+    id: 2,
+    name: "Ilham",
+    surname: "Abasli",
+    age: 20
+
+};
+
+let stu3 = {
+    id: 3,
+    name: "Omer",
+    surname: "Elesgerli",
+    age: 16
+
+};
+
+let stu4 = {
+    id: 4,
+    name: "Semed",
+    surname: "Huseynov",
+    age: 27
+
+};
+
+
+// let result = stur4.name +"-"+ stu4.surname
+
+// console.log(result)
+
+const students = [stu1, stu2, stu3, stu4]
+
+// for (let i = 0; i < students.length; i++) {
+
+//     let result = students[i].name + "-"+ students[i].surname;
+//     console.log(result)
+// }
+
+
+// let sumAge = 0;
+// for (let i = 0; i < students.length; i++) {
+//     sumAge += students[i].age
+// }
+// console.log(sumAge)
+
+
+// let sumAge = 0;
+// for (let i = 0; i < students.length; i++) {
+//     sumAge += students[i].age
+// }
+
+// let averageAge = sumAge/students.length
+
+// if(averageAge>50){
+//     console.log("old")
+// } else{
+//     console.log("young")
+// }
+
+// const elems =[1,3,5,4,6,7];
+
+// for (const item of elems) {
+//     console.log(item)
+// }
+
+// for (const item of students) {
+
+//     console.log(item.name)
+// }
+
+// const product ={
+//     name:"samsung",
+//     price:500
+// };
+
+
+// for (const key in product) {
+
+//     console.log(product[key]) 
+
+// }
+
+
+// for (const stu of students) {
+//     for (const key in stu) {
+//     if(key=="name"){
+//         let datas = key +" "+stu[key];
+//         console.log(datas)
+//     }
+
+//     }
+// }
+
+
+// function showText(){
+//     console.log("test")
+// }
+
+// showText();
+
+
+// function showText(){
+
+//     let isActive = false;
+//     if(isActive){
+//         return;
+//     }
+//     return "tested"
+
+// }
+
+// console.log(showText());
+
+
+// function showName(text){
+// console.log(text)
+// }
+
+// showName("Semed")
+
+
+// function sum(num=100){
+//     console.log(num+200)
+// }
+
+// function getStudentsByAge(students){
+//     let count=0;
+//     for (const stu of students) {
+//         if(stu.age>20){
+//             count++;
+//         }
+//     }
+//     console.log(count)
+// }
+
+// getStudentsByAge(students)
+
+
+
+// function getStudentsByAge(students) {
+//     let count = 0;
+//     for (const stu of students) {
+//         if (stu.age > 20) {
+//             count++;
+//         }
+//     }
+//     count **= 2;
+//     return count;
+// }
+
+// console.log(getStudentsByAge(students));
+
+
+// let elems1 = [1,2,3,4,5]
+// let elems2 = elems1
+// elems2[2] = 200;
+
+// console.log(elems1[2])
+
+
---
+++ scopes-functions-task/assets/js/script.js
@@ -0,0 +1,106 @@
+"use strict"
+
+// function number() {
+//     let n = 21;
+
+//     if (n % 3 === 0 && n % 7 === 0) {
+//         return "the number is divided"
+//     } else {
+//         return "the number is not divisible"
+//     }
+// }
+
+// console.log(number());
+
+
+
+
+// function positiveNumber(num) {
+//     while (num > 0) {
+//         for (let i = num - 1; i > 0; i--) 
+//         {
+//             num *= i;
+//         }
+
+//         return num;
+//     }
+
+// }
+
+// let result = positiveNumber(5)
+
+// console.log(result)
+
+
+
+// let numbers =[1,3,4,5,2,6]
+// let sum = 0;
+
+// function squaresOfEvenNmbers(){
+
+//     for (const item of numbers) {
+//         if(item%2===0){
+//            sum+=item**2;
+//         }
+//     }
+//     return sum;
+// }
+
+// console.log(squaresOfEvenNmbers());
+
+
+
+
+// function login(email, password) {
+//     if (email == "cavid@code.edu.az" && password == "12345"){
+//         return "login success"
+
+//     }else{
+//         return "login unsuccess"
+//     }
+
+// }
+
+// let result = login("t@mail.ru" , "112222")
+
+// console.log(result)
+
+
+
+
+// let nums =[1,3,4,5,2,8]
+// let sumOdd = 0
+// function sumOfOddNumbers(){
+
+//     for (const item of nums) {
+//         if(item%2===1){
+//            sumOdd+=item;
+//         }
+//     }
+    
+//     return sumOdd;
+
+// }
+
+// console.log(sumOfOddNumbers());
+
+
+
+
+
+// let num =[1,3,4,5,2,8,40,12,8]
+// let count = 0
+// function countOfEvenNumbers(){
+
+//     for (const item of num) {
+//         if(item%2===0){
+//            count++;
+//         }
+//     }
+    
+//     return count;
+// }
+
+// console.log(countOfEvenNumbers());
+
+
